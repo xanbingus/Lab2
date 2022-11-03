3cad785863659b0d5b8931b43ab5241d15cdc239
Xan <xantan.21@ichat.sp.edu.sg>
2022-11-03 21:52:02 +0800

    sus

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,43 @@
-print("HELLO")
+def display_main_menu():
+    print("Enter some numbers separated by commas ")
+
+def get_user_input():
+
+    x=input(":")
+    txt=x.split(",")
+    return txt
+
+def calc_average_temperature(array):
+    total=0
+    for i in range (int(len(array))):
+        total=total+int(array[i])
+        average = total/int(len(array))
+    return average
+
+def calc_min_max_temperature(array):
+    max=0
+    min=10000000000000000
+    for i in range(int(len(array))):
+        if int(array[i])>max:
+            max=int(array[i])
+        if int(array[i])<min:
+            min=int(array[i])
+    return max,min
+
+
+
+
+display_main_menu()
+txt=get_user_input()
+average=calc_average_temperature(txt)
+print("Average: "+str(average))
+max,min=calc_min_max_temperature(txt)
+print("Max: "+str(max))
+print("Min: "+str(min))
+
+
+
+
+
+
+
